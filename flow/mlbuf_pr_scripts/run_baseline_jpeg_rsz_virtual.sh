8b862571dd2ab94385ad535fe89284f238ab26e4
ytliu8464 <yil375@ucsd.edu>
2025-09-01 12:32:51 -0700

    modify makefile and add auto scripts

---
+++ flow/mlbuf_pr_scripts/run_baseline_jpeg_rsz_virtual.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export PROJ_DIR="/home/fetzfs_projects/timer_calibration/MLBuf_reproduce/MLBuf_MLCAD"
+export OPENROAD_EXE="${PROJ_DIR}/OR_branch_integration/OpenROAD/build/src/openroad"
+export FLOW_HOME="/home/fetzfs_projects/timer_calibration/ORFS_mlbuf_erc/OpenROAD-flow-scripts/flow"
+
+export DESIGN="jpeg"
+export DESIGN_FULL_NAME="jpeg_encoder"
+export FLOW_VARIANT="rsz_virtual"
+
+export PLATFORM="nangate45"
+export INPUTS_DIR="${PROJ_DIR}/OR_inputs/${DESIGN_FULL_NAME}"
+export GP_ODB="${PROJ_DIR}/scripts/OR_scripts/${DESIGN_FULL_NAME}/${DESIGN_FULL_NAME}_${FLOW_VARIANT}.odb"
+export SKIP_GP_MLBUF="1"
+
+make -f ${FLOW_HOME}/Makefile \
+     DESIGN_CONFIG="${FLOW_HOME}/designs/${PLATFORM}/${DESIGN}/config.mk" \
+     mlbuf_place_and_route
+
+
+
+
+
