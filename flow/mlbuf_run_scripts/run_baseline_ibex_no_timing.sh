fd4edaccf5153b2415d7a39f825cd28ae7e68342
ytliu8464 <yil375@ucsd.edu>
2025-09-02 22:48:41 -0700

    add orfs scripts

---
+++ flow/mlbuf_run_scripts/run_baseline_ibex_no_timing.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export PROJ_DIR="/home/fetzfs_projects/timer_calibration/MLBuf_reproduce/MLBuf_MLCAD"
+export OPENROAD_EXE="${PROJ_DIR}/OR_branch_integration/OpenROAD/build/src/openroad"
+export FLOW_HOME="/home/fetzfs_projects/timer_calibration/ORFS_mlbuf_erc/OpenROAD-flow-scripts/flow"
+
+export DESIGN="ibex"
+export DESIGN_FULL_NAME="ibex"
+export FLOW_VARIANT="no_timing"
+
+export PLATFORM="nangate45"
+export INPUTS_DIR="${PROJ_DIR}/OR_inputs/${DESIGN_FULL_NAME}"
+export GP_ODB="${PROJ_DIR}/scripts/OR_scripts/${DESIGN_FULL_NAME}/${DESIGN_FULL_NAME}_${FLOW_VARIANT}.odb"
+export SKIP_GP_MLBUF="1"
+
+make -f ${FLOW_HOME}/Makefile \
+     DESIGN_CONFIG="${FLOW_HOME}/designs/${PLATFORM}/${DESIGN}/config.mk" \
+     mlbuf_place_and_route
+
+
+
+
+
---
+++ flow/mlbuf_run_scripts/run_mlbuf_jpeg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export PROJ_DIR="/home/fetzfs_projects/timer_calibration/MLBuf_reproduce/MLBuf_MLCAD"
+#export OPENROAD_EXE="${PROJ_DIR}/OR_branch_integration/OpenROAD/build/src/openroad"
+export OPENROAD_EXE="/home/fetzfs_projects/MLBuf/flows/OR_branch_integration/OpenROAD/build_os8/src/openroad"
+export FLOW_HOME="/home/fetzfs_projects/timer_calibration/ORFS_mlbuf_erc/OpenROAD-flow-scripts/flow"
+
+export DESIGN="jpeg"
+export DESIGN_FULL_NAME="jpeg_encoder"
+export FLOW_VARIANT="mlbuf-pretrained"
+
+export PLATFORM="nangate45"
+export INPUTS_DIR="${PROJ_DIR}/OR_inputs/${DESIGN_FULL_NAME}"
+export GP_ODB="${PROJ_DIR}/scripts/OR_scripts/${DESIGN_FULL_NAME}/${FLOW_VARIANT}/${DESIGN_FULL_NAME}_mlbuf.odb"
+export SKIP_GP_MLBUF="1"
+
+make -f ${FLOW_HOME}/Makefile \
+     DESIGN_CONFIG="${FLOW_HOME}/designs/${PLATFORM}/${DESIGN}/config.mk" \
+     mlbuf_place_and_route
